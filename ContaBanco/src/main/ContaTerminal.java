bf4d6b2277f34c4d4d5ce7e6ae694e44eb0d04e5
Augusto Meine <augusto.meine@gmail.com>
2025-06-01 17:55:44 -0300

    Criando o projeto Java

---
+++ ContaBanco/src/main/ContaTerminal.java
@@ -0,0 +1,23 @@
+package main;
+
+public class ContaTerminal {
+	
+	/*
+	
+	 DESAFIO: Ler os dados digitados pelo usuário no terminal e retornar uma frase com os dados notificados.
+	 
+	 Dados)
+	 1)Número da conta (inteiro)
+  	 2)Número da agência (texto)
+     3)Nome do cliente (texto)
+     4)Saldo da conta (decimal)
+     
+     Frase) Olá `NOME_CLIENTE`, obrigado por criar uma conta em nosso banco, sua agência é `AGENCIA`, conta `NUMERO` e seu saldo `SALDO` já está disponível para saque.
+	
+	*/
+	
+	public static void main (String[] args) {
+		
+	}
+	
+}
